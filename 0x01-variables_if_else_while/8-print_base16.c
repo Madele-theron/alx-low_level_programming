9f89219bf25878aa9ede73bfb5b621436f1d26ed
Madele Theron <madele.theron@gmail.com>
2022-07-01 15:36:51 -0700

    Task 8 commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int digit;
+int alpha;
+for (digit = '0'; digit <= '9'; digit++)
+{
+putchar(digit);
+}
+for (alpha = 'a'; alpha <= 'f'; alpha++)
+{
+putchar(alpha);
+}
+putchar('\n');
+return (0);
+}
