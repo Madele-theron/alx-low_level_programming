0f6be71d91e5284b4c961c011f495ad2295e2a2c
Madele Theron <madele.theron@gmail.com>
2022-07-12 08:51:17 -0700

    Task 0 commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *main - Do something
+ *Return: Return something
+ */
+int main(void)
+{
+char *hola = "_putchar";
+int a;
+a = 0;
+while (a < 9)
+{
+putchar(*hola);
+hola++;
+a++;
+}
+putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+# 0x02. C - Functions, nested loops Week 4 at ALX School
+## Tasks
