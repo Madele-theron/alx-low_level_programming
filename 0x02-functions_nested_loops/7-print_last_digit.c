835cd0d3652b82534ff8bbdf048b86c6f91ee581
Madele Theron <madele.theron@gmail.com>
2022-07-13 08:37:21 -0700

    Task 7 commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_last_digit - prints last digit
+* @n: An input integer
+*Return: last digit
+*/
+int print_last_digit(int n)
+{
+return (n % 10);
+}
