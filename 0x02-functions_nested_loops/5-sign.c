e3050d859bea61b0d34a29cc5c1145ff6225dbb4
Madele Theron <madele.theron@gmail.com>
2022-07-13 08:10:58 -0700

    Task 5 commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+* print_sign - prints a sign depending on integer
+* @n: An input integer
+* Return: 1, or 0, or -1
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+putchar('+');
+return  (1);
+}
+else if (n == 0)
+{
+putchar('0');
+return  (0);
+}
+putchar('-');
+return (-1);
+}
