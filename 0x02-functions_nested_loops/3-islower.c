25fda40ce07570d222121c79ade75edaa9ce875f
Madele Theron <madele.theron@gmail.com>
2022-07-13 06:48:29 -0700

    Task 3 commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*_islower - print alphabet lowercase followed by new line
+*Return: 1 if lowercase or 0 if uppercase
+*/
+int _islower(int c)
+{
+for (int i = ‘a’, i <= ‘z’, i++)
+{
+if i == c;
+return (1);
+}
+return (0);
+}
