a15f2dc59a0e17643d8768e0396aa374c9a745be
Madele Theron <madele.theron@gmail.com>
2022-07-13 07:52:02 -0700

    Task 4 re-commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,14 +9,13 @@
 int _isalpha(int c)
 {
 char lower, upper;
-
-for (lower = ‘a’; lower <= ‘z’; lower++)
+for (lower = 'a'; lower <= 'z'; lower++)
 {
-for (upper = ‘A’; upper <= ‘Z’; upper++)
-}
+for (upper = 'A'; upper <= 'Z'; upper++)
+{
 if (c == lower || c == upper)
 return (1);
-{
+}
 }
 return (0);
 }
