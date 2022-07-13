ae31f42c30fdb1736016cd5815d339424c27abf1
Madele Theron <madele.theron@gmail.com>
2022-07-13 07:48:58 -0700

    Task 4 commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*_isalpha - checks for alphabetical letter
+* @c: An input character
+*Return: 1 if c is a letter, 0 otherwise
+*/
+int _isalpha(int c)
+{
+char lower, upper;
+
+for (lower = ‘a’; lower <= ‘z’; lower++)
+{
+for (upper = ‘A’; upper <= ‘Z’; upper++)
+}
+if (c == lower || c == upper)
+return (1);
+{
+}
+return (0);
+}
