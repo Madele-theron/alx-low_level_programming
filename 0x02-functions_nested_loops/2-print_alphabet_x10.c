075687dd4a1c6b84aa4f23cdd3954f6588ac42c5
Madele Theron <madele.theron@gmail.com>
2022-07-13 01:58:11 -0700

    Task 2 commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - print alphabet x 10 followed by new line
+*Return: Always 0.
+*/
+void print_alphabet_x10(void)
+{
+int repeats = 0;
+while (repeats <= 10)
+{
+char alpha = 'a';
+while (alpha <= 'z')
+{
+putchar(alpha);
+alpha++;
+}
+putchar('\n');
+repeats++;
+}
+}
