d7beac322d78e89f55d80ee6e3e23de82f8c5e86
Madele Theron <madele.theron@gmail.com>
2022-07-13 08:29:56 -0700

    Task 6 commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _abs - function that computes the absolute value of an integer
+* @n: An input integer
+*Return: absolute value
+*/
+int _abs(int n)
+{
+if (n < 0)
+{
+return (n * -1);
+}
+else if (n >= 0)
+{
+return (n);
+}
+}
