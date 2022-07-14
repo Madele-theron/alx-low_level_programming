13f07423ed667c60c71a11adf1b417355ea28762
Madele Theron <madele.theron@gmail.com>
2022-07-13 23:41:20 -0700

    Task 8 commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,24 +15,24 @@ for (j = 0; j <= 59; j++)
 if (i < 10)
 {
 putchar('0');
-putchar(i);
+putchar(i + '0');
 }
 else if (i >= 10)
 {
 putchar((i / 10) + '0');
 putchar((i % 10) + '0');
 }
-else if (j < 10)
+if (j < 10)
 {
 putchar(':');
 putchar('0');
-putchar(i);
+putchar(j + '0');
 }
 else if (j >= 59)
 {
 putchar(':');
-putchar((i / 10) + '0');
-putchar((i % 10) + '0');
+putchar((j / 10) + '0');
+putchar((j % 10) + '0');
 }
 putchar('\n');
 }
