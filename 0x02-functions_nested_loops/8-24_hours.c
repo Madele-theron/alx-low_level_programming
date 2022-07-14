1aaa722feb382914aa90442bc7cf9031b6026f3e
Madele Theron <madele.theron@gmail.com>
2022-07-13 23:38:43 -0700

    Task 8 commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* jack_bauer - prints every minute of the day
+* Return: Always 0
+*/
+void jack_bauer(void)
+{
+int i, j;
+for (i = 0; i <= 23; i++)
+{
+for (j = 0; j <= 59; j++)
+{
+if (i < 10)
+{
+putchar('0');
+putchar(i);
+}
+else if (i >= 10)
+{
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+}
+else if (j < 10)
+{
+putchar(':');
+putchar('0');
+putchar(i);
+}
+else if (j >= 59)
+{
+putchar(':');
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+}
+putchar('\n');
+}
+}
+}
