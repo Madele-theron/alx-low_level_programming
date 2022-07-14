a4f89dbca0c6f26341d3d07dc6615c64714266ee
Madele Theron <madele.theron@gmail.com>
2022-07-14 00:12:37 -0700

    Task 9 commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* times_table - prints every minute of the day
+* Return: Always 0
+*/
+void times_table(void)
+{
+int row, col, multi;
+for (row = 0; row <= 9; row++)
+{
+for (col = 0; col <= 9; col++)
+{
+multi = row * col;
+if (multi == 0)
+{
+putchar(multi + '0');
+}
+else if (multi < 10)
+{
+putchar(',');
+putchar(' ');
+putchar(multi + '0');
+}
+else if (multi >= 10)
+{
+putchar(',');
+putchar(' ');
+putchar((multi / 10) + '0');
+putchar((multi % 10) + '0');
+}
+}
+putchar('\n');
+}
+}
