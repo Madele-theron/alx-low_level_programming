f02ef32391969144d4a3d5b2bad6f80b35cd4b90
Madele Theron <madele.theron@gmail.com>
2022-07-14 00:46:03 -0700

    Task 10 commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* add - adds two integers
+*@i: input integer
+*@j: input integer
+* Return: Result of addition
+*/
+int add(int i, int j)
+{
+return (i + j);
+}
