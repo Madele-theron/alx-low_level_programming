d9633561815896e9d9a928aa43e08226bbadaad1
Madele Theron <madele.theron@gmail.com>
2022-07-14 01:41:40 -0700

    Task 11 commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_to_98 - prints all numbers till 98
+* @n: input integer
+* Return: Always 0
+*/
+void print_to_98(int n)
+{
+int n = 111;
+if (n < 98)
+{
+while (n < 98)
+{
+printf("%d, ", n);
+n++;
+}
+printf("98\n");
+}
+else if (n > 98)
+{
+while (n > 98)
+{
+printf("%d, ", n);
+n--;
+}
+printf("98\n");
+}
+}
